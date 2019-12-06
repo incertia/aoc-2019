1ce5cff5c6406d6c259457709bd4ed8fc801e872
Will Song <incertia@incertia.net>
2019-12-06 11:53:26 -0500

    cleanup

--- src/AOC006.hs
+++ src/AOC006.hs
@@ -1,43 +1,42 @@
+{-# LANGUAGE TupleSections #-}
+
 module AOC006 
   ( solve006
   ) where
 
-import Control.Lens (ix, (^?!))
-import Data.HashMap.Strict hiding (filter)
+import Data.Foldable (foldl')
+import Data.Hashable (Hashable)
 import Data.List.Split (splitOn)
-import Data.List (nub)
-import Data.Maybe (fromMaybe)
-import Data.Monoid (mempty, mconcat, (<>))
+import Data.Monoid (mempty, (<>))
+import Data.Tree (Tree(Node), unfoldTree)
 
 import qualified Data.HashMap.Strict as HM
 import qualified Data.HashSet as HS
 
-type OrbitMap = HM.HashMap String String
-type Graph = HM.HashMap String (HS.HashSet String)
+type Graph a b = HM.HashMap a (HS.HashSet b)
 
 solve006 :: String -> Bool -> Integer
 solve006 i b =
   let o = orbits <$> lines i
-      ot = HM.fromList o
-      vs = nub $ (fst <$> o) ++ (snd <$> o)
-      g = HM.fromList $ (\v -> (v, matches v o)) <$> vs
-  in  if b then bfs (HS.fromList ["YOU"]) "SAN" g - 2
-      else sum . fmap (olen ot) $ HM.keys ot
-
-olen :: OrbitMap -> String -> Integer
-olen _ "COM" = 0
-olen o x = 1 + olen o (o ^?! ix x)
+      ot = unfoldTree (\x -> (x,) $ snd <$> filter ((==x) . fst) o) "COM"
+      g = HM.fromList . HS.toList . HS.map (\v -> (v, matches v o))
+        . HS.fromList $ (fst <$> o) ++ (snd <$> o)
+  in  if b then bfs (HS.singleton "YOU") "SAN" g - 2
+      else sum . toDepth 0 $ ot
 
 orbits :: String -> (String, String)
-orbits x = let [a, b] = splitOn ")" x in (b, a)
+orbits x = let [a, b] = splitOn ")" x in (a, b)
 
-matches :: String -> [(String, String)] -> HS.HashSet String
+toDepth :: Integer -> Tree a -> Tree Integer
+toDepth d (Node a ts) = Node d (toDepth (d + 1) <$> ts)
+
+matches :: (Hashable a, Eq a) => a -> [(a, a)] -> HS.HashSet a
 matches v o = HS.fromList $ l ++ r
   where l = snd <$> filter ((==v) . fst) o
         r = fst <$> filter ((==v) . snd) o
 
-bfs :: HS.HashSet String -> String -> Graph -> Integer
+bfs :: (Hashable a, Eq a) => HS.HashSet a -> a -> Graph a a -> Integer
 bfs s e g = if e `HS.member` s then 0
             else 1 + bfs t e h
-  where t = s <> mconcat (fromMaybe mempty . flip HM.lookup g <$> HS.toList s)
-        h = g `HM.difference` HM.fromList (zip (HS.toList s) (repeat ()))
+  where t = foldl' (<>) mempty . HS.map (flip (HM.lookupDefault mempty) g) $ s
+        h = HM.filterWithKey (\k _ -> not $ k `HS.member` s) g
