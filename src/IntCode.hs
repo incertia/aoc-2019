6e3e78a0894660b116068b5b93a7bcb51754a491
Will Song <incertia@incertia.net>
2019-12-07 10:24:37 -0500

    optimize state and add some types for debugging

--- src/IntCode.hs
+++ src/IntCode.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoMonadFailDesugaring #-}
+{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TemplateHaskell #-}
+
 module IntCode
   ( toTape, eval
   , TapeMachine, initialMachine
@@ -8,16 +11,29 @@ module IntCode
   , readPure, readT, writeT
   ) where
 
-import Control.Lens (makeLenses, at, ix, use, preuse, uses, over, _2, (^.), (.=), (%=), (+=), (?=))
+import Control.Lens (makeLenses, at, ix, use, preuse, uses, over, _2, (&), (^.), (^?!), (.~), (%~), (?~), (.=), (%=), (+=), (?=))
 import Control.Monad (when)
 import Control.Monad.State.Strict (MonadState)
 import Data.Has (Has, hasLens)
-import Data.HashMap.Strict (HashMap, fromList)
+import Data.Hashable (Hashable)
+import Data.HashMap.Strict (HashMap)
+import Data.HashSet (HashSet)
 import Data.Maybe (fromMaybe)
 import Data.Vector (Vector)
+import GHC.Generics (Generic)
+
+import qualified Data.HashMap.Strict as HM (fromList)
+import qualified Data.HashSet as HS (fromList)
 
 data IntCode = OpAdd | OpMul | OpInput | OpOutput | OpJumpT | OpJumpF | OpLT | OpEQ | Halt
-  deriving (Show, Eq)
+  deriving (Show, Eq, Generic)
+data Breakpoint = BreakInsn   Integer
+                | BreakOpcode IntCode
+  deriving (Show, Eq, Generic)
+
+instance Hashable IntCode
+instance Hashable Breakpoint
+
 type Tape = HashMap Integer Integer
 
 data TapeMachine = TapeMachine { _tapePC    :: Integer
@@ -26,15 +42,20 @@ data TapeMachine = TapeMachine { _tapePC    :: Integer
                                , _tapeOut   :: [Integer]
                                , _tape      :: Tape
                                , _numInputs :: Int
-                               , _numInsns  :: Int }
+                               , _numInsns  :: Int
+                               , _breaks    :: HashSet Breakpoint
+                               }
   deriving (Show, Eq)
 makeLenses ''TapeMachine
 
 toTape :: String -> Tape
-toTape x = let y = read $ "[" ++ x ++ "]" in fromList $ zip [0..fromIntegral (length y)] y
+toTape x = let y = read $ "[" ++ x ++ "]" in HM.fromList $ zip [0..] y
 
 initialMachine :: [Integer] -> Tape -> TapeMachine
-initialMachine i t = TapeMachine 0 i 0 [] t 0 0
+initialMachine i t = TapeMachine 0 i 0 [] t 0 0 []
+
+debugMachine :: [Integer] -> Tape -> [Breakpoint] -> TapeMachine
+debugMachine i t bp = TapeMachine 0 i 0 [] t 0 0 (HS.fromList bp)
 
 decode :: (Has TapeMachine s, MonadState s m) => m (IntCode, [Integer], [Integer], Integer)
 decode = do
@@ -70,7 +91,6 @@ input = do
   p <- use $ hasLens . tapeInPos
   r <- preuse $ hasLens . tapeIn . ix p
   hasLens . tapeInPos += 1
-  hasLens . numInputs += 1
   return $ fromMaybe (error $ "Ran out of input at position " ++ show p) r
 
 output :: (Has TapeMachine s, MonadState s m) => Integer -> m ()
@@ -86,21 +106,31 @@ writeT :: (Has TapeMachine s, MonadState s m) => Integer -> Integer -> m ()
 writeT i x = hasLens . tape . at i ?= x
 
 eval :: (Has TapeMachine s, MonadState s m) => m ()
-eval = do
+eval = evalDebug False
+
+evalDebug :: (Has TapeMachine s, MonadState s m) => Bool -> m ()
+evalDebug debug = do
   (op, pa:_:pc:_, a:b:_, ip) <- decode
-  hasLens . numInsns += 1
+  when debug $ hasLens . numInsns += 1
   if op == Halt then return ()
   else do
-     case op of
-          OpAdd   -> writeT pc $ a + b
-          OpMul   -> writeT pc $ a * b
-          OpInput -> writeT pa =<< input
-          OpLT    -> writeT pc . fromIntegral . fromEnum $ a < b
-          OpEQ    -> writeT pc . fromIntegral . fromEnum $ a == b
-          _       -> pure ()
-     hasLens . tapePC .= case op of
-                              OpJumpT -> if a /= 0 then b else ip
-                              OpJumpF -> if a == 0 then b else ip
-                              _       -> ip
-     eval
-     when (op == OpOutput) $ output a
+    let modifyState s = s & writeP s
+                          & tapeInPos %~ (if op == OpInput then succ else id)
+                          & tapePC .~ npc
+                          & numInputs %~ (if op == OpInput && debug then succ else id)
+        writeP s =
+          case op of
+               OpAdd   -> tape . at pc ?~ a + b
+               OpMul   -> tape . at pc ?~ a * b
+               OpInput -> tape . at pa ?~ (s ^?! tapeIn . ix (s ^. tapeInPos))
+               OpLT    -> tape . at pc ?~ (fromIntegral . fromEnum $ a < b)
+               OpEQ    -> tape . at pc ?~ (fromIntegral . fromEnum $ a == b)
+               _       -> id
+        npc =
+          case op of
+               OpJumpT -> if a /= 0 then b else ip
+               OpJumpF -> if a == 0 then b else ip
+               _       -> ip
+    hasLens %= modifyState
+    evalDebug debug
+    when (op == OpOutput) $ output a
