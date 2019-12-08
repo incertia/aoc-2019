059f9b4bb4cf1058ada12238c09f85db3082de34
Will Song <incertia@incertia.net>
2019-12-08 18:32:24 -0500

    optimization: mark each seg with the current distance so we can immediately compute distances in `intersect` (to make this directly comparable to c)

--- src/AOC003.hs
+++ src/AOC003.hs
@@ -9,26 +9,25 @@ import Text.Read (Read(..), get)
 
 data Vec = U Integer
          | R Integer
-data Seg = H (Integer, Integer) Integer
-         | V (Integer, Integer) Integer
+data Seg = H (Integer, Integer) Integer Integer
+         | V (Integer, Integer) Integer Integer
 
-intersect :: Seg -> Seg -> Maybe (Integer, Integer)
-intersect (H (x1, y1) l1) (V (x2, y2) l2) =
-  if inrange x2 x1 (x1 + l1) && inrange y1 y2 (y2 + l2) then Just (x2, y1) else Nothing
-intersect v@V{} h@H{} = h `intersect` v
-intersect _ _ = Nothing
+intersect :: Bool -> Seg -> Seg -> Maybe Integer
+intersect z (H (x1, y1) l1 d1) (V (x2, y2) l2 d2) =
+  if inrange x2 x1 (x1 + l1) && inrange y1 y2 (y2 + l2) then Just $
+     if z then d1 + d2 + abs (x1 - x2) + abs (y1 - y2)
+     else abs x2 + abs y1
+  else Nothing
+intersect z v@V{} h@H{} = intersect z h v
+intersect _ _ _ = Nothing
 
 instance Read Vec where
   readPrec = get >>= flip fmap readPrec . (\case {'U' -> U; 'D' -> U . negate; 'L' -> R . negate; 'R' -> R})
 
-mkSegs :: (Integer, Integer) -> [Vec] -> [Seg]
-mkSegs _ [] = []
-mkSegs p@(x, y) (U z : vs) = V p z : mkSegs (x, y + z) vs
-mkSegs p@(x, y) (R z : vs) = H p z : mkSegs (x + z, y) vs
-
-dist :: [Seg] -> (Integer, Integer) -> Integer
-dist (H (x, y) l : s) p@(x', y') = if y == y' && inrange x' x (x + l) then abs (x - x') else abs l + dist s p
-dist (V (x, y) l : s) p@(x', y') = if x == x' && inrange y' y (y + l) then abs (y - y') else abs l + dist s p
+mkSegs :: (Integer, Integer) -> Integer -> [Vec] -> [Seg]
+mkSegs _ _ [] = []
+mkSegs p@(x, y) d (U z : vs) = V p z d : mkSegs (x, y + z) (d + abs z) vs
+mkSegs p@(x, y) d (R z : vs) = H p z d : mkSegs (x + z, y) (d + abs z) vs
 
 inrange :: Integer -> Integer -> Integer -> Bool
 inrange x a b = x >= min a b && x <= max a b
@@ -36,8 +35,6 @@ inrange x a b = x >= min a b && x <= max a b
 solve003 :: String -> Bool -> String
 solve003 i z = show $
   let [l1, l2] = lines i
-      a = mkSegs (0, 0) $ read $ "[" ++ l1 ++ "]"
-      b = mkSegs (0, 0) $ read $ "[" ++ l2 ++ "]"
-      f = if z then \p -> dist a p + dist b p
-          else \(a, b) -> abs a + abs b
-  in  minimum . fmap f . catMaybes $ intersect <$> a <*> b
+      a = mkSegs (0, 0) 0 $ read $ "[" ++ l1 ++ "]"
+      b = mkSegs (0, 0) 0 $ read $ "[" ++ l2 ++ "]"
+  in  minimum . catMaybes $ intersect z <$> a <*> b
